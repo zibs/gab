7e9e0b3c9d6bd76a8baa97457860e38c06234bba
zibs <elizibin@gmail.com>
2016-03-29 15:58:59 -0700

    Style login/logout password reset/edit signup pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable
-         
+
   VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true,
-          uniqueness: true,
+  validates :email, uniqueness: true,
           format: VALID_EMAIL_REGEX
 
 end
